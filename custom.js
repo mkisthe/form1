ed128bc60399bb30ab03651fcd2b4c28679a712a
Mitva Kariya <108940119+mkisthe@users.noreply.github.com>
2023-09-27 18:35:06 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,45 @@
+    $("#formValidation").validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        phone: {
+          required: true,
+          minlength: 10
+        },
+        subject: {
+          required: true
+        },
+        message: {
+          required: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Please enter your name",
+          minlength: "Name must be at least 2 characters"
+        },
+        email: {
+          required: "Please enter your email address",
+          email: "Please enter a valid email address"
+        },
+        phone: {
+          required: "Please enter your phone number",
+          minlength: "Phone number must be at least 10 digits"
+        },
+        subject: {
+          required: "Please enter a subject"
+        },
+        message: {
+          required: "Please enter a message"
+        }
+      },
+      submitHandler: function(form) {
+        form.submit();
+      }
+    });
